58bace4fd57b4a6ec0e149ad04c0085de2ba175d
Focinfi <focinfi@gmail.com>
2016-08-08 11:49:27 +0800

    chore(model): Add columns for User

--- app/models/model.go
+++ app/models/model.go
@@ -1,11 +1,1 @@
 package models
-
-import (
-	"time"
-)
-
-// Model for base model
-type Model struct {
-	ID        string    `json:"user_id"`
-	CreatedAt time.Time `json:"created_at"`
-}
--- app/models/user.go
+++ app/models/user.go
@@ -1,9 +1,17 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 // User for users
 type User struct {
-	Model
-	Name string `json:"name"`
+	gorm.Model
+	UUID     string `json:"uuid" gorm:"type:char(32)"`
+	UserName string `json:"user_name" gorm:"unique_index"`
+	Name     string `json:"name"`
+	Email    string `json:"email" gorm:"type:varchar(100);unique_index"`
+	Phone    string `json:"phone" gorm:"type:varchar(20);unique_index"`
 }
 
 // DisplayName implements Auth interface
